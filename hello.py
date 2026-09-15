e722956b4ff22cb004eaa49f3449212f6cd37b92
sintocs <moninroma2410@gmail.com>
2026-09-15 19:23:13 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,5 +15,38 @@ def Mean(X,Y):
     GMean = (X * Y) ** 0.5
     return Amean, GMean
 
-print(Mean(4, 6))
+print(Mean(4, 6))#Proc 3
+
+import math
+def  TrianglePS(a):
+    P = 3 * a
+    S = a ** 2 * math.sqrt(3 / 4)
+    return P , S
+
+print(TrianglePS(3))#Proc 4
+
+def RectPS(x1, y1, x2, y2):
+    P = (abs(x1 - x2) + abs(y1 - y2)) * 2
+    S = abs(x1 - x2) * abc(y1 - y2)
+    return P , S
+
+print(RectPS(6, 2 , 8 , 3))#Proc 5
+
+
+
+
+    def DigitCoutSum(K):
+        count = 0
+        sum = 0
+        for i in str(K):
+            count += 1
+            sum += int(i)
+    
+        return count, sum  
+    print(DigitCoutSum(1234567))#Proc 6
+
+
+
+
+    
 
